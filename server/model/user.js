4fb41356b16ac5396e67570a854072d2337c3b34
Sarrveshhh <sarrvesharyaa@gmail.com>
2024-02-23 22:03:14 +0530

    converted the data into model reable format

--- server/model/user.js
+++ server/model/user.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const customerSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    // required: true,
   },
   gender: {
     type: String,
